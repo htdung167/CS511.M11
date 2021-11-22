9b89083643f5c2801aa9d5cfe7278a28dc6f9702
trongtn2110 <19522410@ms.uit.edu.vn>
2021-11-22 23:09:19 +0700

    doi ten

--- PhienDich/PhienDich/Form1.Designer.cs
+++ PhienDich/PhienDich/Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace PhienDich
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel_main.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,7 +41,7 @@ namespace PhienDich
             // 
             this.panel_main.Controls.Add(this.label1);
             this.panel_main.Location = new System.Drawing.Point(10, 11);
-            this.panel_main.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel_main.Margin = new System.Windows.Forms.Padding(2);
             this.panel_main.Name = "panel_main";
             this.panel_main.Size = new System.Drawing.Size(332, 264);
             this.panel_main.TabIndex = 0;
@@ -55,15 +56,6 @@ namespace PhienDich
             this.label1.TabIndex = 0;
             this.label1.Text = "Dũng đẹp trai ta";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(538, 307);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(377, 35);
@@ -73,16 +65,32 @@ namespace PhienDich
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(485, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Trognj ạkdakjdk";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 304);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(902, 533);
-            this.Controls.Add(this.label2);
             this.ClientSize = new System.Drawing.Size(676, 433);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel_main);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "PhienDich";
             this.panel_main.ResumeLayout(false);
@@ -98,6 +106,7 @@ namespace PhienDich
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
